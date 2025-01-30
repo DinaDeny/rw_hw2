e865c18e1bad7e370a7ae8c2c0ff89fe0d65c3b2
Dina Denysenko <dina.denysenko@gen.tech>
2025-01-30 12:16:44 +0200

    Add new function

---
+++ index.js
@@ -0,0 +1,16 @@
+function myFunction(p1, p2) {
+    return p1 * p2;
+  }
+
+
+
+
+  function toCelsius(fahrenheit) {
+    return (5/9) * (fahrenheit-32);
+  }
+
+  console.log(myFunction(5,6))
+  
+  console.log(toCelsius(77));
+
+
